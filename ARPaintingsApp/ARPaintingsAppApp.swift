1d5c7d13aa3e9c1d9b2d35fc87658f59b4a8e8d8
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-12-04 15:27:54 -0800

    Initial Commit

---
+++ ARPaintingsApp/ARPaintingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARPaintingsAppApp.swift
+//  ARPaintingsApp
+//
+//  Created by Ariel Ortiz on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ARPaintingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
